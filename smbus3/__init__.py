61ffe922e24483f28696e252f04d54b9a58af9ed
Elliott Indiran <eindiran@users.noreply.github.com>
2024-06-08 12:23:10 -0400

    Remove inline LICENSE info and improve some minor doc gen stuff

--- smbus3/__init__.py
+++ smbus3/__init__.py
@@ -1,26 +1,8 @@
-"""smbus3 - A drop-in replacement for smbus-cffi/smbus-python"""
-# The MIT License (MIT)
-# Copyright (c) 2020 Karl-Petter Lindegaard
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+"""
+smbus3 - A drop-in replacement for smbus2/smbus-cffi/smbus-python
+"""
 
 from .smbus3 import I2cFunc, SMBus, i2c_msg
 
-__version__ = "0.4.3"
+__version__ = "0.5.0"
 __all__ = ["SMBus", "i2c_msg", "I2cFunc"]
